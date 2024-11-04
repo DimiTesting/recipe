84d295f03e2b0ecde72b9554d8f19fc36d15b5eb
amankeldi.kurban <dimitesting31@gmail.com>
2024-11-04 18:41:55 +0200

    Setting up a global exception handler

--- Program.cs
+++ Program.cs
@@ -9,10 +9,20 @@ namespace Recipe
     {
         public static void Main(string[] args)
         {
-            ConsoleReader consoleReader = new ConsoleReader();
-            WorkWithFile workWithFile = new WorkWithFile();
-            ConsoleLogic consoleLogic = new ConsoleLogic(consoleReader, workWithFile);
-            consoleLogic.CreateRecipe();
+            try
+            {
+                ConsoleReader consoleReader = new ConsoleReader();
+                WorkWithFile workWithFile = new WorkWithFile();
+                ConsoleLogic consoleLogic = new ConsoleLogic(consoleReader, workWithFile);
+                consoleLogic.CreateRecipe();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Sorry! The application experienced an unexpected error and will be closed" + 
+                "The error message is" + ex.Message);
+                Console.WriteLine("Press any key to close.");
+                Console.ReadKey();
+            }
         }
     }
 }
--- obj/Debug/net8.0/Recipe.AssemblyInfo.cs
+++ obj/Debug/net8.0/Recipe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c420b4bd99a040b515fd75a9fe1d33224c1d3e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a0cc5714271ae831be069571ed4c7dfb42f703")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
